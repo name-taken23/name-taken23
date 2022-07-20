3250e5ac9dc90539ff12efaf61aeee9c34d33d5d
James Reed <James.Reed@netmatters-scs.com>
2022-07-20 15:19:47 +0100

    Sticky navbar on scroll, content collapsible, minor features

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,85 @@
 //Form validation.
 
 
-$('.contact-btn').on('click',(e)=> {
+$('.contact-btn').on('click', (e) => {
     e.preventDefault();
     let contactForm = {
         fname: document.forms["contact-form"]["fname"].value,
         lname: document.forms["contact-form"]["lname"].value,
         emailAdd: document.forms["contact-form"]["email"].value,
         emailSubject: document.forms["contact-form"]["emailsubject"].value
-    
-    }   
-       for(let x in contactForm){
-        if(contactForm[x] === ""){
-           
-           console.log(x);
-            switch(x){
+
+    }
+    for (let x in contactForm) {
+        if (contactForm[x] === "") {
+
+
+            switch (x) {
                 case "fname":
                     x = "First Name.";
                     break;
                 case "lname":
                     x = "Last Name.";
                     break;
-                case "emailAdd": 
+                case "emailAdd":
                     x = "Email Address.";
                     break;
                 case "emailSubject":
                     x = "Email Subject.";
-                    
+
             }
+            console.log(x);
             alert('You are missing the field: ' + x);
-            
+
         }
     }
     const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    
-    if(regex.test(contactForm.emailAdd) == false){
-        alert("Please enter a valid email");
-    }else{
-        alert("Form submitted!");
+
+    if (regex.test(contactForm.emailAdd) == false) {
+        alert("Please enter a valid email.");
+    } else {
+        alert("Form submitted.");
     }
+});
+
+
+//navbar scroll
+$(window).on('scroll', () => {
+    let bannerH = $('.banner-wrapper').height() - 100;
+    if (document.body.scrollTop > bannerH || document.documentElement.scrollTop > bannerH) {
+        document.getElementById("custom-navbar").style.top = "0";
+    } else {
+        document.getElementById("custom-navbar").style.top = "-100px";
+    }
+});
+//navbar hide on window resize.
+$(window).resize(() => {
+    if ($(this).width() >= 981) {
+        $('.custom-navbar').show();
+    }
+    else {
+        $('.custom-navbar').hide();
+    }
+});
+
+
+//collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+$(window).on('scroll', function () {
+    let bannerH = $('.banner-wrapper').height() - 350;
+    console.log(document.documentElement.scrollTop);
+        for (i = 0; i < coll.length; i++) {
+            coll.item(0).classList.toggle("active");
+            var content = coll.item(0).nextElementSibling;
+            if (document.documentElement.scrollTop < bannerH) { 
+
+                content.style.maxHeight = null;
+            } else {
+                
+                content.style.display = "flex"
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        }
+    
 });
